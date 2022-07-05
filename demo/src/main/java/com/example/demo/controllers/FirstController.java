f8a7b30591633e6966dd2aa309627c4d2ffda66b
Pranati Nayak <FRL@kmd.dk>
2022-07-05 14:55:02 +0530

    first commit

---
+++ demo/src/main/java/com/example/demo/controllers/FirstController.java
@@ -0,0 +1,12 @@
+package com.example.demo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World RESTful with Spring Boot";
+    }  
+}
